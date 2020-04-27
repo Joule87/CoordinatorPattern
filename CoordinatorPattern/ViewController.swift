071856488e7582eab3da92d21d87a1fd6b762b99
Julio Collado <jcollado@altimetrik.com>
2020-04-27 09:34:31 -0300

    Initial Commit

---
+++ CoordinatorPattern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoordinatorPattern
+//
+//  Created by Julio Collado on 4/27/20.
+//  Copyright © 2020 julio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
